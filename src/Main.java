fdb9cd1ae658c7eae4859253ad047487c717acef
Sajer24 <sajer.kanat1992@gmail.com>
2022-10-03 23:48:19 +0300

    Дз легкий уровень

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Pushkin", "Alexander");
+        Author author2 = new Author("Lermontov", "Mikhail");
+        Book book1 = new Book("TheTaleOfTheGoldenCockerel", author1, 1834);
+        Book book2 = new Book("Borodino", author2, 1837);
+        System.out.println(book1);
+        System.out.println(book2);
+        book2.setPublicationYear(1999);
+        System.out.println(book2);
     }
 }
